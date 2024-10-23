affb1f005972c1c6f009def2f54405815d313513
Adam Wong <adam.wong789@googlemail.com>
2024-10-23 22:46:21 +0100

    Added Navigation buttons to Header

--- drinked-app/src/components/header.tsx
+++ drinked-app/src/components/header.tsx
@@ -2,10 +2,28 @@ import SidebarButton from './sidebarbutton';
 
 export default function Header() {
     return (
-        <div className="midbar bg-blue-300 text-slate-50 pt-4 pb-4">
+        <div className="midbar bg-blue-300 text-slate-50 pt-4">
             <SidebarButton />
-            <div className="border text-center">
-            Test Header
+            <div className="grid grid-cols-3">
+                <div className='col-start-2 text-center'>
+                    <h1>Drinked</h1>
+                </div>
+            </div>
+            <div/>
+            <div/>
+            <div className="grid grid-cols-4 text-center">
+                <div className="border">
+                    Dashboard
+                </div>
+                <div className="border">
+                    Add Drink
+                </div>
+                <div className="border">
+                    About
+                </div>
+                <div className="border">
+                    Login
+                </div>
             </div>
         </div>
     )
