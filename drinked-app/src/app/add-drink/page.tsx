6b0c259901a96c25097355cd433d47be7dd8035f
Adam Wong <adam.wong789@googlemail.com>
2024-11-01 01:24:08 +0000

    Added functionality to choose and display a drink

--- drinked-app/src/app/add-drink/page.tsx
+++ drinked-app/src/app/add-drink/page.tsx
@@ -1,5 +1,7 @@
 
 import Image from 'next/image';
+import {loadDrinks, getDrink, setDrink} from '../data-access/drink';
+import { revalidatePath } from 'next/cache';
 
 export default async function AddDrink() {
 
@@ -9,7 +11,9 @@ export default async function AddDrink() {
     }
 
     const data = await getData();
-    console.log(data);
+
+    const catalogue = await loadDrinks();
+    const drink = await getDrink(catalogue, "0");
     return (
         <div className='midbar min-h-screen'>
         <main className='col-start-2 text-center'>
@@ -18,9 +22,20 @@ export default async function AddDrink() {
                 <div className='col-start-1 flex flex-col'>
                     <div className='p-3 mt-10 bg-blue-200 rounded-full'>Your Drinks</div>
                     {/* <ul> >myDrinks.map(()=>{<li>myDrinks.stuff</li>}) </ul>*/}
+                    {drink.name}
                 </div>
                 <div className='col-start-2 flex flex-col'>
                     <button className='p-3 mt-10 bg-blue-200 rounded-full'> Add new drink</button>
+                    <form action = {async (formData: FormData)=>{
+                        'use server';
+                        const newDrinkId = formData.get('id') as string;
+                        await setDrink(catalogue, newDrinkId);
+                        revalidatePath('/add-drink');
+                    }}>
+                        <input type='text' name='id' placeholder='Enter drink id'/>
+                        <button type='submit'>Add</button>
+                    </form>
+
                     <div className='p-3 mt-10 bg-blue-200 rounded-full'>Recent Drinks</div>
                     <div className='w-80 h-80'>
                         <Image
--- drinked-app/src/app/data-access/drink.ts
+++ drinked-app/src/app/data-access/drink.ts
@@ -1,5 +1,3 @@
-'use server'
-
 import { promises as fs } from 'fs';
 
 export type Drink = {
@@ -10,17 +8,29 @@ export type Drink = {
     description: string;
 };
 
+(global as any).drink = {
+    id: "0",
+    name: "Asahi Super Dry",
+    category: "Beer",
+    abv: 5.0,
+    description: "Japanese rice lager with a clean, crisp and refreshing taste."
+} as Drink;
+
 export async function loadDrinks(){
     const file = await fs.readFile(process.cwd() + '/public/drink_catalogue.json', 'utf-8');
     return JSON.parse(file).drinks as Drink[];
 }
 
 export async function getDrink(catalogue: Drink[], drinkId: string){
+    return (global as any).drink;
+}
+
+export async function setDrink(catalogue: Drink[], drinkId: string){
     for (var drink of catalogue){
         console.log(drink.id + " -> " + drinkId)
         if (drink.id === drinkId){
-            return drink as Drink;
+            (global as any).drink = drink
+            break;
         }
     }
-    return catalogue[0] as Drink;
 }
